e5c8169e66d75415ad48b8b99597d9e60345c4d8
bossthanawat <boss41680@gmail.com>
2021-06-04 14:54:57 +0700

    add

--- src/views/First/index.tsx
+++ src/views/First/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import useTheme from "hooks/useTheme";
 import styled from "styled-components";
-import { useTranslation } from "react-i18next";
-import { Button } from "components/Button";
-import { types } from "contexts/ThemeContext/type";
 import PageLayout from "components/layout/Page"
 import SyText from "components/Text"
 const Page = styled(PageLayout)`
@@ -21,21 +17,11 @@ const Text = styled(SyText)`
 `;
 
 function First() {
-  const { changeTheme } = useTheme();
-  const { t, i18n } = useTranslation();
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-  };
 
   return (
     <>
       <Page>
-        <Text fontSize="28px" bold>{t("title")}</Text>
-        <Text></Text>
-        <Button onClick={() => changeTheme(types.DARK)} scale={"md"}>{t("DARK")}</Button>
-        <Button onClick={() => changeTheme(types.LIGHT)} scale={"md"}>{t("LIGHT")}</Button>
-        <Button onClick={() => changeLanguage("en")}>EN</Button>
-        <Button onClick={() => changeLanguage("th")}>TH</Button>
+        <Text fontSize="28px" bold>FIRST</Text>
       </Page>
     </>
   );
